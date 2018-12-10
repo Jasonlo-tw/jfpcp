5c90c388b957bb92b04b65101348692e69f74be8
Kevin Hou <neonnova916@gmail.com>
2018-12-10 20:02:31 +0800

    initial setup with webpacker and routes

---
+++ config/routes.rb
@@ -0,0 +1,58 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # TODO:   the routes here are all replaced with "resources", should be replaced to correct ones when the planning is done.
+
+  
+  root 'pages#home' # use vue for search functionality on layout
+
+  # Account utility
+  resources :account do
+    get :login
+    get :register
+  end
+
+  # Collections
+  resources :collections do
+    resources :all_products do 
+      get :backpack
+      get :shoulder_bag
+      get :handbag
+      get :purse
+      get :gift_under_100
+      
+    end
+    get :influencer_collection
+    get :graffiti_collection
+    
+  end
+
+  # Static pages
+  resources :pages do
+    get :about_us
+    get :graffiti_world
+    get :faqs # use Vue here
+    get :delivery_region
+    get :contact_us
+    get :return
+    get :privacy_policy
+    get :terms_of_use
+    get :dealer_list
+  end
+
+  # Blog pages, essentially static (or generated with editor?)
+  # TODO: blog editor?
+  resources :blogs do
+    get :lookbook
+    get :news
+  end
+
+  # Checkout process
+  resources :cart do
+    get :checkout
+  end
+
+# Admin functionality
+  resources :admin
+
+end
