baf276ce3f468d8cdf3b84ac8baf516fd3f5de09
Kevin Hou <neonnova916@gmail.com>
2018-12-22 23:14:39 +0800

    Product can be shown with correct @product that titleizes params[:product_name]

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,31 @@
 class ProductsController < ApplicationController
 
     def show
-        @product = Product.find_by(product_name: params[:product_name])
+        @product = Product.find_by(product_name: titleized_param)
     end
+
+    private
+
+    
+
+
+    # titleize param[:product_name] before defining @product to get eg.
+    # Brown Rice Spaceman Backpack
+    # instead of 
+    # brown-rice-spaceman-backpack
+    # as originally in params[:product_name]
+    def titleized_param
+        
+        params[:product_name].titleize
+    end
+
 end
+
+
+# original show:
+# def show
+#   @product = Product.find_by(product_name: params[:product_name])
+# end
+
+# Should turn params[:product_name] back to original 
+# eg. "brown-rice-spaceman-backpack" -> "Brown Rice Spaceman Backpack"
