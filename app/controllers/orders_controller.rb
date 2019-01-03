03728fbd8b884630af8e7fb19b9c56c995df13a7
Kevin Hou <neonnova916@gmail.com>
2019-01-03 17:32:04 +0800

    makeup

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
+  
+  
+  # Usually there shoould be a #new, but as the first step of checkout is writing contact info, it's ok to just use #contact
   def contact
+    @order = Order.new
   end
 
   def shipping_payment
