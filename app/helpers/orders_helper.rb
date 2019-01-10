821055ac0ad557bccbb8fda85e30295be173e525
Kevin Hou <neonnova916@gmail.com>
2019-01-10 21:21:30 +0800

    Disable some sections

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -3,7 +3,8 @@ module OrdersHelper
         # TODO: correct the logic later. Using "Payment pending" or not as condition for now.
         
 
-        if !@orders.empty? && @orders.first.process == "Payment pending"
+        if !@orders.empty? && @orders.find(id).process == "Payment pending"
+            
             link_to 'Cancel Order', {controller: "orders", action: "update", order_id: id, process: "Order cancelled"}, method: :put, data: { confirm: "Are you sure to cancel?\n The order will still be kept in record." }
         elsif
             link_to 'Return & Refund', '#'
